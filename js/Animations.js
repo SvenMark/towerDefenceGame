ff65bae9fcf916982231795fd7f75e1b870371d6
kevin <kevinsnijder9@Hotmail.com>
2017-10-31 00:15:00 +0100

    Removed comments

--- js/Animations.js
+++ js/Animations.js
@@ -1,38 +1,3 @@
-/*$(document).ready(function() {
-    $('#instructioncontent').hide();
-    $('#topbar').animate({
-        top: '0px'
-    }, 700);
-    $('#bottombar').delay(500).animate({
-        top: '90vh'
-    }, 1000);
-});
-$( "#buttonleft" ).click(function() {
-    $('#topbar').animate({
-        top: '-440px'
-    }, 700);
-    $('#bottombar').animate({
-        top: '100vh'
-    }, 700);
-    $('#midbar').slideUp(400);
-    $('.fullscreen').slideUp(1000);
-});
-$( "#buttonright" ).click(function() {
-    $('#instructioncontent').show();
-    $('#topbar').animate({
-        top: '-440px'
-    }, 700);
-    $('#bottombar').animate({
-        top: '100vh'
-    }, 700);
-    $('#midbar').slideUp(400);
-    $('#welcomescreen').slideUp(1000);
-});
-$( "#instrplay" ).click(function() {
-    $('#instructions').slideUp(1000);
-});*/
-
-
 function placeshow(){
     $('#placetowerdiv').fadeIn(300);
     $('#placetowertxt').delay(300).animate({"width":"92%"}, 600);
